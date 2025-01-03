eaf7535a3d7f60c9a6f19eb67eea35cfc2b12f41
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-01-03 13:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ function updateSizeInfo() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const sizeInfoDiv = document.getElementById('sizeInfo');
+      sizeInfoDiv.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+    }
+
+    updateSizeInfo();
+    window.addEventListener('resize', updateSizeInfo);
